e8bc34a0de8eb5bc03916fc2d9423c5bb00f7e36
Nkosiyapha <110283495+realnkosi@users.noreply.github.com>
2025-12-04 11:09:08 +0100

    Resolve merge conflict again

--- Main.java
+++ Main.java
@@ -2,14 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!\nThis in Nkosiyapha's Java Program");
 
-<<<<<<< HEAD
-        String[] fruits = { "Mango", "Banana", "Orange", "Apple", "Grapes", "Pineapple" };
+        String[] fruits = { "Mango", "Banana", "Orange", "Apple", "Grapes", "Pineapple", "Passion Fruit" };
 
         // add sorting logic here, quick sort algorithm
         quickSort(fruits, 0, fruits.length - 1);
-=======
-        String[] fruits = { "Mango", "Banana", "Orange", "Apple", "Grapes", "Passion Fruit" };
->>>>>>> d2d628231048d3fe4e3167769e5c84bc35465c2e
 
         for (String fruit : fruits) {
             System.out.println(fruit);
