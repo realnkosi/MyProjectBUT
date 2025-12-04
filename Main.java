f78d1ffa2bc009818afe5610841e3f798586507f
Nkosiyapha <110283495+realnkosi@users.noreply.github.com>
2025-12-04 11:04:18 +0100

    Added another fruit

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!\nThis in Nkosiyapha's Java Program");
 
-        String[] fruits = { "Mango", "Banana", "Orange", "Apple", "Grapes" };
+        String[] fruits = { "Mango", "Banana", "Orange", "Apple", "Grapes", "Pineapple" };
 
         // add sorting logic here, quick sort algorithm
         quickSort(fruits, 0, fruits.length - 1);
