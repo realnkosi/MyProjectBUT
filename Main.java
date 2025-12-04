b316819eade20453e9301e2d27bff2718c193d12
Nkosiyapha <110283495+realnkosi@users.noreply.github.com>
2025-12-04 10:28:31 +0100

    Add basic table + print elements

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!\nThis in Nkosiyapha's Java Program");
+
+        String[] fruits = { "Mango", "Banana", "Orange", "Apple", "Grapes" };
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
