17287ecdad24b3279a165356ea9b333c946bcd52
Nkosiyapha <110283495+realnkosi@users.noreply.github.com>
2025-12-04 10:31:43 +0100

    Add random initialization

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.println("Hello World!\nThis in Nkosiyapha's Java Program");
 
         String[] fruits = { "Mango", "Banana", "Orange", "Apple", "Grapes" };
+
         for (String fruit : fruits) {
             System.out.println(fruit);
         }
